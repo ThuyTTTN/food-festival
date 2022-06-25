be93ec2dfc73cb1518903fbe3d87057547277195
Thuy Nguyen <ttruong06@gmail.com>
2022-06-24 21:43:20 -0400

    service worker

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,14 @@ $(document).ready(function() {
     {
       title: "We travel all over the US",
       subtitle: "Check out our schedule!",
-      img: "./assets/img/food-table.jpg",
+      img: img1,
       btnText: "View Schedule",
       btnUrl: "schedule.html"
     },
     {
       title: "Our food is seriously the bomb!",
       subtitle: "What are you waiting for?",
-      img: "./assets/img/grill.jpg",
+      img: img2,
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
